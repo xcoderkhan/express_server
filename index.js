4291a57722af53069e4e91c26f52857922138dd2
xCoder <xcoder@xCoders-iMac.local>
2023-06-08 18:45:51 +0600

    u

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello this is my first server');
 })
 
+app.get('/data', (req, res) => {
+    res.send('New data coming soon');
+})
+
 app.listen(port, () => {
     console.log(`This is express port ${port}`)
 })
